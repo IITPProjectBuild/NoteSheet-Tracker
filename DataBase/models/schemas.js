b58b50f364c2f84dc5f427addcf52aa792af8778
Arpit-Raj1 <arpitraj4114@gmail.com>
2024-07-08 13:15:43 +0530

    added node_modules to .gitignore changed all occurences of intendor to user in db schema

--- DataBase/models/schemas.js
+++ DataBase/models/schemas.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 // For Users
-const IndentorSchema = new Schema({
-  indentorId: ObjectId, // UserId
-  indentorName: String, //Username
+const UserSchema = new Schema({
+  userId: ObjectId, // UserId
+  userName: String, //Username
   email: String,
   passwordHash: String,
   passwordSalt: String,
@@ -17,12 +17,12 @@ const IndentorSchema = new Schema({
 });
 
 // Updates updatedAt
-IndentorSchema.pre('save', function (next) {
+UserSchema.pre('save', function (next) {
   this.updatedAt = Date.now();
   next();
 });
 
-const Indentor = mongoose.model('Indentor', IndentorSchema);
+const User = mongoose.model('User', UserSchema);
 
 //For Clubs and Events aka Organization
 const OrgSchema = new Schema({
@@ -61,7 +61,7 @@ const Temp = mongoose.model('Temp', TempSchema);
 // For Templates that will be saved
 const TempSavedSchema = new Schema({
   tempId: ObjectId,
-  indentorId: ObjectId,
+  userId: ObjectId,
   orgId: ObjectId,
   status: String, // e.g., 'Draft', 'Submitted', 'Approved', 'Rejected'
   createdAt: { type: Date, default: Date.now }, //ISODate
@@ -71,7 +71,7 @@ const TempSavedSchema = new Schema({
 });
 // Do we also have to include which level or by whom it is rejected by ? // Swagatam: We should do this ig
 // we can do it as
-// approvedBy: [{ type: ObjectId, ref: 'Indentor' }] // Those who have approved it will be in this array
+// approvedBy: Shema.Types.Array // Those who have approved it will be in this array
 
 TempSavedSchema.pre('save', function (next) {
   this.updatedAt = Date.now();
@@ -82,7 +82,7 @@ const TempSaved = mongoose.model('TempSaved', TempSavedSchema);
 
 // For NoteSheet
 const NoteSheetSchema = new Schema({
-  indentorId: ObjectId,
+  userId: ObjectId,
   noteSheetId: ObjectId,
   // user_id: ObjectId,  User id was two times in the framework why??
   action: String, // e.g., 'Submitted', 'Approved', 'Rejected'
@@ -95,7 +95,7 @@ const NoteSheet = mongoose.model('NoteSheet', NoteSheetSchema);
 
 // I have no idea wtf it is no 1
 const NoteSheetApprovedSchema = new Schema({
-  intendorId: ObjectId,
+  userId: ObjectId,
   noteSheetId: ObjectId,
   approverId: ObjectId,
   status: String, // e.g., 'Forwarded', 'Rejected'
@@ -108,7 +108,7 @@ const NoteSheetApproved = mongoose.model('NoteSheetApproved', NoteSheetApprovedS
 
 //  Its to late i cant figure this out
 const NoteSheetStatusSchema = new Schema({
-  intendorId: ObjectId,
+  userId: ObjectId,
   // user_id ObjectId, why are ther two ids here??
   noteSheetId: ObjectId,
   remainderDate: { type: Date, default: Date.now }, // Swagatam: What's the purpose of remainderDate here ??
@@ -121,7 +121,7 @@ const NoteSheetStatus = mongoose.model('NoteSheetStatus', NoteSheetStatusSchema)
 
 // For Fund Tracking
 const FundSchema = new Schema({
-  intendorId: ObjectId,
+  userId: ObjectId,
   orgId: ObjectId,
   noteSheetId: ObjectId,
   amountSpend: Number,
