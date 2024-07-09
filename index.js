800b6a28d07066bde749a9bfa7421900d146f57e
Swagatam Pati <swagatamheroz@outlook.com>
2024-07-09 11:59:58 +0530

    fix: mongo connection, ESModules

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
-const express = require('express');
+import mongoose from 'mongoose';
+import express from 'express';
 
-const IndentorSchema = require('./DataBase/models/schemas.js');
-
-let conn = mongoose.connect('mongodb://localhost:27017/project');
+const dbUrl = 'mongodb://127.0.0.1:27017/noteSheetTracker';
+mongoose.connect(dbUrl)
+  .then(() => {
+    console.log('Mongo connected successfully!');
+  })
+  .catch(error => {
+    console.log('Error: Couldn\'t connect to mongo!');
+    console.log(error);
+  });
 
 const app = express();
 const port = 3000;
