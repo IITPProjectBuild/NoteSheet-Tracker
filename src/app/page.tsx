4121221573649dcf3d0fffced59e4e51d3b09649
Arpit-Raj1 <arpitraj4114@gmail.com>
2024-08-21 19:34:31 +0530

    reverted to  4ae7ef7344b651a760640174a7c9d9a44bbf7d18 "fixed pending sheets"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
     return (
         <>
-            <Link href="login">hello</Link>
+            <Link href="Login">hello</Link>
         </>
     );
 }
